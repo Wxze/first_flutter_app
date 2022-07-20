4cecef38b803af42a93e00d21f2d42597255f6cc
Wxze <gabrieljose528@gmail.com>
2022-07-20 17:25:23 -0300

    Added AppBar and Centered Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World')
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('My First App'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://img.freepik.com/free-photo/adorable-brown-white-basenji-dog-smiling-giving-high-five-isolated-white_346278-1657.jpg?w=1380&t=st=1658348042~exp=1658348642~hmac=470983f84f29cc676b2e7c017d906bd432bd0d584ce03841e208b6cf4152308e'),
+          ),
+        ),
       ),
     ),
   );
